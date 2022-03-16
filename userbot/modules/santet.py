689746f823818bfcf28a5c9633eb45f8bfdd2162
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-17 03:18:25 +0700

    bH

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -1,14 +1,13 @@
 from time import sleep
-from userbot import CMD_HELP, ALIVE_NAME
-from userbot.events import register
+from userbot import CMD_HELP, owner, CMD_HANDLER as cmd
+from userbot.utils import ram_cmd, edit_or_reply
 
 
-@register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit(f"`{ALIVE_NAME} Berusaha Menyantet Jamet Ini`")
+@ram_cmd(pattern=r'^\.santet(?: |$)(.*)')
+async def _(event):
+    typew = await edit_or_reply(event, f"`{owner} Berusaha Menyantet Sikontol Ini`")
     sleep(2)
-    await typew.edit("`Mencari Nama Jamet Ini...`")
+    await typew.edit("`Mencari Nama Yang Haram Untuk Di basmi...`")
     sleep(1)
     await typew.edit("`Santet Online Segera Dilakukan,Mampus Lu!!`")
     sleep(1)
@@ -317,7 +316,7 @@ async def typewriter(typew):
 
 
 CMD_HELP.update({
-    'santetonline':
-    '`.santet`\
-        \nUsage: Santet Online Buat Bercanda.'
+    "santet":
+    f"`{cmd}santet`\
+        \nUsage: Santet Online Buat Bercanda."
 })
