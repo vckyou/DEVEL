91a912388fbc311f2e99decaaf55b44877274086
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-18 03:08:25 +0700

    Update rmemes.py

--- userbot/modules/rmemes.py
+++ userbot/modules/rmemes.py
@@ -2541,7 +2541,7 @@ async def _(event):
         await event.edit("`\"Love is a terrifying thing\nIt’s not safe. Because when you love someone, you have to face the fact that you can lose them.\nSometimes life throws an unexpected wrench in your way. It might be that you’re in jeopardy of losing your career, your freedom, or worst of all, you might even find out that a loved one has died\nThese things make you realize how precious life is, how important every second we have on this earth is, and how important the people we care about are to us.\"`")
 
 
-@ram_cmdpattern="^.kiss(?: |$)(.*)")
+@ram_cmd(pattern="^.kiss(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
