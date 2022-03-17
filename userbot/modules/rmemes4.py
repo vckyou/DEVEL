cda3e977e1383bb8ae62bda72daa702757506b1e
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-17 16:14:48 +0700

    Update rmemes4.py

--- userbot/modules/rmemes4.py
+++ userbot/modules/rmemes4.py
@@ -323,7 +323,7 @@ async def _(gmyut):
 
 
 @ram_cmd(pattern="tolol(?: |$)(.*)")
-async def _(tolol)
+async def _(tolol):
      typew = await edit_or_reply(tolol, "`TOLOL...`")
     sleep(2)
     await typew.edit("`Pertama Kamu tolol....`")
