a4910641b0f20ee487ced2ce3c0352667f2e7be3
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-17 19:05:25 +0700

    Update raos.py

--- userbot/modules/raos.py
+++ userbot/modules/raos.py
@@ -2,7 +2,7 @@ from platform import uname
 from userbot import CMD_HELP, BLACKLIST_CHAT, CMD_HANDLER as cmd
 from userbot.utils import edit_or_reply, ram_cmd
 from userbot.events import register
-from pyrogRam.string import *
+from pyrogRam.string import pyrogRam
 
 eor = edit_or_reply
 
