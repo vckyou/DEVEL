030dd598dd903e8d1e0970580721122505877a0c
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-18 18:54:12 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -137,7 +137,7 @@ SUDO_HANDLER = os.environ.get("SUDO_HANDLER", r"$")
 # default no leave
 BLACKLIST_CHAT = os.environ.get("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001692751821]
+    BLACKLIST_CHAT = [-1001692751821, -1001459812644]
 
 # only developer
 OWNDEV = os.environ.get("OWNDEV", None)
